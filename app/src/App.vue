f63ab30b89033f23a281bfdca1de5cd82dec2a0d
Vladislav <vladkoval039@gmail.com>
2024-05-15 14:32:00 +0300

    добавление внутр  файлов кода

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,1 +1,23 @@
-//Основной компонент приложения, точка входа для приложения Vue.js
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+      </aside>
+      <div class="content">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        posts: []
+      }
+    }
+  }
+</script>
--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import Router from 'vue-router';
 Vue.config.roductionTip = false;
 
+const router = new Router({
+    routes: [
+      {
+ 
+      }
+    ]
+   })
+
 new Vue({
-    render: h => h(App)
+    render: h => h(App),
+    router
 }).$mount("#app");
