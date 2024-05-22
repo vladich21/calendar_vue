0d19be6e9a8495916f40595a169289899be8c80c
Andrey Breykin <ABreykin@inno.tech>
2024-05-22 20:40:41 +0300

    Добавление компонента Sidebar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignUp from '../views/SignUp.vue'
 import SignIn from '../views/SignIn.vue'
+import Main from '../views/Main.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'main',
-      redirect: 'sign-up'
+      component: Main
     },
     {
       path: '/sign-up',
---
+++ src/views/Main.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Sidebar from '../components/Sidebar.vue'
+
+</script>
+
+<template>
+    <Sidebar />
+</template>
+
+<style lang="scss"></style>
