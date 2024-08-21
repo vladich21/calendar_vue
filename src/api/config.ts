6db1ee320c613f5a39f3e2a414d0e63d07ac01c0
Vladislav <vladkoval039@gmail.com>
2024-08-21 23:28:12 +0300

    Ветка для настройки Api и fix

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,16 @@
-import axios from "axios";
+import axios, { AxiosResponse } from 'axios'
 
 export const mainApi = axios.create({
-    baseURL: 'api'
+  baseURL: 'http://127.0.0.1:5173/api'
 })
+type PostRegistration = {
+  email: string
+  password: string
+  First_name: string
+  Last_name: string
+}
+export const fetchPostRegistration = async (
+  requestBody: PostRegistration
+): Promise<AxiosResponse<void>> => {
+  return await mainApi.post('/registration', requestBody)
+}
